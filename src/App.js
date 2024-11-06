cd08ad75b05a439144176ed9c3688cedcf005832
wliuye <161380356+wliuye@users.noreply.github.com>
2024-11-05 20:25:28 -0800

    fixed errors

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-import SearchEngine from "./SearchEngine";
 
 function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
-      <SearchEngine />
     </div>
   );
 }
